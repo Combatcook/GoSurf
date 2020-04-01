f71c3b9dc9c1b335cd521b82c0fcd14cafe88319
Povarov <cook0208@mail.ru>
2020-04-01 16:40:55 +0300

    start

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 $(function() {
     
-
-
-
+  $('.header__slider').slick({
+    infinite: true,
+    fade: true,
+    prevArrow: '<img class="slider-arrow slider-arrow__left" src="img/arrow-left.svg" alt="arrow">',
+    nextArrow: '<img class="slider-arrow slider-arrow__right" src="img/arrow-right.svg" alt="arrow">',
+  });
 
 });
