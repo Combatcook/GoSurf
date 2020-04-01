038f9fd4a4e6236dd307c3e9c51d1e488a2449f4
Povarov <cook0208@mail.ru>
2020-04-01 18:22:22 +0300

    header almost done

--- js/main.js
+++ js/main.js
@@ -5,6 +5,13 @@ $(function() {
     fade: true,
     prevArrow: '<img class="slider-arrow slider-arrow__left" src="img/arrow-left.svg" alt="arrow">',
     nextArrow: '<img class="slider-arrow slider-arrow__right" src="img/arrow-right.svg" alt="arrow">',
+    asNavFor: '.slider-dots',
+  });
+
+  $('.slider-dots').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: '.header__slider',
   });
 
 });
