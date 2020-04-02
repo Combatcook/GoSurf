30dbeb66d80ca07b21d2913066ad0ceb11328cde
Povarov <cook0208@mail.ru>
2020-04-02 22:17:48 +0300

    surf-slider + titles

--- js/main.js
+++ js/main.js
@@ -14,4 +14,11 @@ $(function() {
     asNavFor: '.header__slider',
   });
 
+  $('.surf-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<img class="slider-arrow slider-arrow__left" src="img/arrow-left.svg" alt="arrow">',
+    nextArrow: '<img class="slider-arrow slider-arrow__right" src="img/arrow-right.svg" alt="arrow">',
+  });
+
 });
