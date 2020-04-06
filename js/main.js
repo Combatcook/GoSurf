7b68ceb0911135118a8561d2241369e211bbec02
Povarov <cook0208@mail.ru>
2020-04-06 20:09:14 +0300

    travel slider

--- js/main.js
+++ js/main.js
@@ -43,4 +43,11 @@ $(function() {
     asNavFor: '.surf-slider',
   });
 
+  $('.travel__slider').slick({
+    infinite: true,
+    fade: true,
+    prevArrow: '<img class="slider-arrow slider-arrow__left" src="img/arrow-left.svg" alt="arrow">',
+    nextArrow: '<img class="slider-arrow slider-arrow__right" src="img/arrow-right.svg" alt="arrow">',
+  });
+
 });
