2ef7c52705f57ca299ab3a24f244d5d5d0348b77
Povarov <cook0208@mail.ru>
2020-04-17 00:11:25 +0300

    sleep is done

--- js/main.js
+++ js/main.js
@@ -50,4 +50,51 @@ $(function() {
     nextArrow: '<img class="slider-arrow slider-arrow__right" src="img/arrow-right.svg" alt="arrow">',
   });
 
+  // for input-number
+  $('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="img/plus.svg" alt="plus"></div><div class="quantity-button quantity-down"><img src="img/minus.svg" alt="minus"></div></div>').insertAfter('.quantity input');
+  $('.quantity').each(function() {
+    var spinner = $(this),
+      input = spinner.find('input[type="number"]'),
+      btnUp = spinner.find('.quantity-up'),
+      btnDown = spinner.find('.quantity-down'),
+      min = input.attr('min'),
+      max = input.attr('max');
+
+    btnUp.click(function() {
+      var oldValue = parseFloat(input.val());
+      if (oldValue >= max) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue + 1;
+      }
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
+    });
+
+    btnDown.click(function() {
+      var oldValue = parseFloat(input.val());
+      if (oldValue <= min) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue - 1;
+      }
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
+    });
+
+  });
+
+  //calculator
+  $('.quantity-button').on('click', function(){
+    var parents = $(this).parents('.holder-slider__info');
+    let sum = $('.sum', parents).data('nights') * $('.nights', parents).val() + $('.sum', parents).data('guests') * $('.guests', parents).val();
+    $('.sum', parents).html('$' + sum);
+  });
+    
+  $('.quantity').each(function() {
+    var parents = $(this).parents('.holder-slider__info');
+    let sum = $('.sum', parents).data('nights') * $('.nights', parents).val() + $('.sum', parents).data('guests') * $('.guests', parents).val();
+    $('.sum', parents).html('$' + sum);
+  });
+
 });
